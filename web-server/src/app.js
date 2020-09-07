223607cf84bf2332251c56282fe23d0724cbc0d6
alex kazanski <alex1kazanski@gmail.com>
2020-09-07 08:21:46 -0400

    feat(styling): hbs pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,51 +1,59 @@
-const path = require('path')
-const express = require('express')
-const hbs = require('hbs')
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
 
-const app = express()
+const app = express();
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
-app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Weather',
-        name: 'Alex Kazanski'
-    })
-})
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Alex Kazanski",
+  });
+});
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Me',
-        name: 'Alex Kazanski'
-    })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Alex Kazanski",
+  });
+});
 
-app.get('/help', (req, res) => {
-    res.render('help', {
-        helpText: 'This is some helpful text.',
-        title: 'Help',
-        name: 'Alex Kazanski'
-    })
-})
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some helpful text.",
+    title: "Help",
+    name: "Alex Kazanski",
+  });
+});
 
-app.get('/weather', (req, res) => {
-    res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia'
-    })
-})
+app.get("/weather", (req, res) => {
+  res.send({
+    forecast: "It is snowing",
+    location: "Philadelphia",
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", { errorMessage: "Help article not found", name: "Alex Kazanski" });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", { errorMessage: "Help page not found", name: "Alex Kazanski" });
+});
 
 app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
-})
+  console.log("Server is up on port 3000.");
+});
